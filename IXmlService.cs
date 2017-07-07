dae0950fc037b55cfc9f13f901d634e4ce0c0886
Harith Neralla <hneralla56@gmail.com>
2017-07-06 20:31:06 -0700

    added empty REST services

---
+++ IXmlService.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace XmlOps
+{
+    [ServiceContract]
+    public interface IXmlService
+    {
+        // Takes a URL for XML validation
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        string verification(string xmlUrl, string xslUrl); 
+
+        // Transforms the XML to HTML using the XSL
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        string transformation(string xmlUrl, string xslUrl); 
+
+        // Takes a URL and returns the node's content information related to the key.
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        string[] search(string xmlUrl, string key); 
+
+        // Returns the path expression value of the given path.
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        string XPathSearch(string xmlUrl, string path); 
+    }
+}
---
+++ XmlService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XmlOps
+{
+    public class XmlService : IXmlService
+    {
+        // Takes a URL for an XML document and an XML Schema document and verifies them. Returns error information.
+        public string verification(string xmlUrl, string xsdUrl)
+        {
+            string output = " "; // default output 
+
+            return output;
+        }
+
+        // Takes URLs for XML and XSL documents and transforms the XML into HTML. Returns HTML in the form of a string.
+        public string transformation(string xmlUrl, string xslUrl)
+        {
+            string output = " "; // default output
+
+            return output;
+        }
+
+        // Takes a URL and returns the node's content information related to the key.
+        public string[] search(string xmlUrl, string key)
+        {
+            List<string> output = new List<string>(); // default output
+
+            return output.ToArray<string>();
+        }
+
+        // Returns the path expression value of the given path.
+        public string XPathSearch(string xmlUrl, string path)
+        {
+            string output = " "; // default output
+
+            return output;
+        }
+    }
+}
